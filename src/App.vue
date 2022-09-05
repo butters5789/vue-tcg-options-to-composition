b47d4fca10cb1ce23da84a688a081e90a145d623
butters5789 <butters5789@gmail.com>
2022-09-05 13:17:24 -0600

    built with options api

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3 v-if="showGoals">{{ goal }}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toggleGoals">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showGoals: true,
+      goal: 'Complete the course and master Vue.',
+    };
+  },
+  methods: {
+    toggleGoals() {
+      this.showGoals = !this.showGoals;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
